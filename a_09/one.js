82c272de3855bb2e809dc5435d451db48f50ba0f
ankitmishra2232 <mca.2125@unigoa.ac.in>
2022-03-18 09:02:34 +0530

    .

--- a_09/one.js
+++ a_09/one.js
@@ -1,37 +1,33 @@
+var multi = document.getElementById('multiS');
+var caseradio = document.getElementById('caseradio');
+var Case = document.getElementById('case');
+var reg = document.getElementById('reg');
 
 function highlight() {
    var opar = document.getElementById('paragraph').innerHTML;
-    // alert("bhKK");
   var paragraph = document.getElementById('paragraph');
   var search = document.getElementById('Stext').value.trim();
-//   alert("bhKK");
-  search = search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');  // $& means the whole matched string
-//   alert("bhKK");
-	
-	var multi = document.getElementById('multiS');
-	
-	var Case = document.getElementById('case');
-	if(Case.checked==true && multi.checked==true)
-		var repl = new RegExp(`${search}`,'gi');
-	else if(Case.checked==true && multi.checked==false)
-		var repl = new RegExp(`${search}`,'i');
-	else if(Case.checked==false && multi.checked==true)
-		var repl = new RegExp(`${search}`,'g');
-	if(Case.checked==false && multi.checked==false)
-		var repl = new RegExp(`${search}`);
-//   alert("bhKK");
-//    if (search.length > 0)
-	paragraph.innerHTML = opar.replace(repl, match => `<span class='new1'>${match}</span>`);
-//    else paragraph.innerHTML = opar;
-	// paragraph.innerHTML = opar.replace(repl, "");
-//alert("hi");
+	if(reg.checked==false){
+		search = search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');  // $& means the whole matched string	
+		if(Case.checked==true && multi.checked==true)
+			var repl = new RegExp(`${search}`,'gi');
+		else if(Case.checked==true && multi.checked==false)
+			var repl = new RegExp(`${search}`,'i');
+		else if(Case.checked==false && multi.checked==true)
+			var repl = new RegExp(`${search}`,'g');
+		if(Case.checked==false && multi.checked==false)
+			var repl = new RegExp(`${search}`);
+		paragraph.innerHTML = opar.replace(repl, match => `<span class='new'>${match}</span>`);
+	}
+	else{
+		var result = new RegExp(`${search}`,'gi');
+		paragraph.innerHTML = opar.replace(result ,match => `<span class='new'>${match}</span>`);
+	}
+  
 }
 
 function unchecked(){
-	var multi = document.getElementById('multiS');
-	var caseradio = document.getElementById('caseradio');
-	var Case = document.getElementById('case');
-	var reg = document.getElementById('reg');
+
 	if(reg.checked==true){
 		Case.disabled=true;
 		multi.disabled=true;
